046026c8f23fba0250ef724fc90775d18362b9e4
Dina Essam <dina.essam.fcis@gmail.com>
2021-08-07 04:47:30 +0200

    Product Details

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -9,22 +9,35 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit {
   product:any;
-
+  images:any[] = [];
   constructor(private productService:ProductService ,private router:Router,private activatedroute: ActivatedRoute) { 
 
     const productid = this.activatedroute.snapshot.paramMap.get('id');
     this.productService.getProductsById({prodNo:productid}).subscribe(
       (result) =>{
         if(result.result.status == '200')
-        
+        {
           this.product = result.data.product;
+          this.images = this.product.images;
+          this.product.prodColorList.forEach(color => {
+
+            this.images.push(
+              {
+                imgPath:color.colorImg
+              }
+            );
+            
+          });
+        }
         else
           this.router.navigate(['/dashboard/materials']);
       });
 
+
   }
 
   ngOnInit(): void {
+    
   }
 
 }
