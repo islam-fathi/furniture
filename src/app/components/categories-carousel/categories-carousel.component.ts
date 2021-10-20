2c68ab4b20fe8828fa794a179d26ec638184379b
islam-fathi <efathi456@gmail.com>
2021-10-20 18:46:24 +0200

    responsive updated

--- src/app/components/categories-carousel/categories-carousel.component.ts
+++ src/app/components/categories-carousel/categories-carousel.component.ts
@@ -5,64 +5,76 @@ import { CategoryService } from 'src/app/services/category/category.service';
 @Component({
   selector: 'app-categories-carousel',
   templateUrl: './categories-carousel.component.html',
-  styleUrls: ['./categories-carousel.component.css']
+  styleUrls: ['./categories-carousel.component.css'],
 })
 export class CategoriesCarouselComponent implements OnInit {
+  categories: any = [];
 
-  categories: any =[];
+  constructor(
+    private categoryServise: CategoryService,
+    private router: Router
+  ) {}
 
-  constructor(private categoryServise:CategoryService, private router:Router) { }
+  slideConfig = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    swipeToSlide: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
-  // slides = [
-  //   {img: "../../../assets/images/category2.jpg"},
-  //   {img: "../../../assets/images/home-bg.jpg"},
-  //   {img: "../../../assets/images/category2.jpg"},
-  //   {img: "../../../assets/images/home-bg.jpg"}
-  // ];
-
-  slideConfig = {"slidesToShow": 3, "slidesToScroll": 1};
-
-  // addSlide() {
-  //   this.slides.push({img: "http://placehold.it/350x150/777777"})
-  // }
-  
   removeSlide() {
     this.categories.length = this.categories.length - 1;
   }
-  
+
   slickInit(e) {
     console.log('slick initialized');
   }
-  
+
   breakpoint(e) {
     console.log('breakpoint');
   }
-  
+
   afterChange(e) {
     console.log('afterChange');
   }
-  
+
   beforeChange(e) {
     console.log('beforeChange');
   }
 
   ngOnInit(): void {
-    this.categoryServise.getCategories().subscribe(
-      (result) =>{
-        if(result.result.status == '200')
-          this.categories = result.data.categoryList;
-      }
-    );
+    this.categoryServise.getCategories().subscribe((result) => {
+      if (result.result.status == '200')
+        this.categories = result.data.categoryList;
+    });
   }
 
-  onImgError($event)
-  {
-    $event.target.src = "assets/images/placeholder-image.png";
+  onImgError($event) {
+    $event.target.src = 'assets/images/placeholder-image.png';
   }
 
-  deatilsCategory(categoryNo)
-  {
-    this.router.navigate(['/categories/'+categoryNo]);
+  deatilsCategory(categoryNo) {
+    this.router.navigate(['/categories/' + categoryNo]);
   }
-
 }
--- src/app/components/winter-sale-carousel/winter-sale-carousel.component.ts
+++ src/app/components/winter-sale-carousel/winter-sale-carousel.component.ts
@@ -3,44 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-winter-sale-carousel',
   templateUrl: './winter-sale-carousel.component.html',
-  styleUrls: ['./winter-sale-carousel.component.css']
+  styleUrls: ['./winter-sale-carousel.component.css'],
 })
 export class WinterSaleCarouselComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   slides = [
-    {img: "../../../assets/images/PNG content/chair1.png"},
-    {img: "../../../assets/images/PNG content/chair1.png"},
-    {img: "../../../assets/images/PNG content/chair1.png"},
-    {img: "../../../assets/images/PNG content/chair1.png"}
+    { img: 'assets/images/PNG content/chair1.png' },
+    { img: 'assets/images/PNG content/chair1.png' },
+    { img: 'assets/images/PNG content/chair1.png' },
+    { img: 'assets/images/PNG content/chair1.png' },
   ];
 
-  slideConfig = {"slidesToShow": 1, "slidesToScroll": 1};
+  slideConfig = { slidesToShow: 1, slidesToScroll: 1 };
 
-  
   removeSlide() {
     this.slides.length = this.slides.length - 1;
   }
-  
+
   slickInit(e) {
     console.log('slick initialized');
   }
-  
+
   breakpoint(e) {
     console.log('breakpoint');
   }
-  
+
   afterChange(e) {
     console.log('afterChange');
   }
-  
+
   beforeChange(e) {
     console.log('beforeChange');
   }
 
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
